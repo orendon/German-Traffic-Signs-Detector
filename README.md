ab86e4237f0b279ecbe302e41b140c55caa0665a
Oscar Rendon <orendon@gmail.com>
2018-05-16 19:32:22 -0500

    Improve CLI app with required arguments

--- README.md
+++ README.md
@@ -10,5 +10,40 @@ More details on `requirements.txt` file, main packages are:
 - Click
 - tensorflow
 - scikit-learn
+- scikit-image
+- opencv-python
 - jupyter
 - scipy
+
+## Implemented models
+
+### model1 - Logistic Regression using Scikit-learn
+More details on the report page.
+
+### model2 - Logistic Regression using Tensorflow
+More details on the report page.
+
+### model3 - LeNet-5 using Tensorflow
+Based on [Yann Lecun paper](http://yann.lecun.com/exdb/publis/pdf/lecun-01a.pdf), more details on the report page.
+
+## CLI usage
+
+Download the traffic signs dataset:
+```shell
+python app.py download
+```
+
+Train `model1` specifying the training images folder:
+```shell
+python app.py train -m model1 -d images/train
+```
+
+Test `model1` specifying the test images folder:
+```shell
+python app.py test -m model1 -d images/test
+```
+
+Run inferences on `model1` and specifying a custom set of images folder:
+```shell
+python app.py infer -m model1 -d images/test
+```
--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import click
 
 import kiwi.dataset as dataset
+import kiwi.dataloader as dataloader
+
+MY_MODELS = ['model1', 'model2', 'model3']
 
 # download command
+##################
 @click.group()
 def data():
   pass
@@ -12,28 +16,33 @@ def download():
   dataset.process()
 
 # train command
+###############
 @click.group()
 def training():
   pass
 
 @training.command()
-@click.option('-m')
-@click.option('-d')
+@click.option('-m', required=True, type=click.Choice(MY_MODELS))
+@click.option('-d', required=True)
 def train(m, d):
   """Usage: train -m MODEL -d FOLDER """
-  click.echo('training model %s on folder %s ' % (m, d))
+  # click.echo('Training model %s on folder %s ' % (m, d))
+  X_data, Y_data = dataloader.from_folder(d)
+  print("Loaded %s images by %s datapoints each" % X_data.shape)
 
 # test command
+##############
 @click.group()
 def testing():
   pass
 
 @testing.command()
-@click.option('-m')
-@click.option('-d')
+@click.option('-m', required=True, type=click.Choice(MY_MODELS))
+@click.option('-d', required=True)
 def test(m, d):
   """Usage: test -m MODEL -d FOLDER """
   click.echo('testing model %s on folder %s ' % (m, d))
+
 
 cli = click.CommandCollection(sources=[data, training, testing])
 if __name__ == '__main__':
